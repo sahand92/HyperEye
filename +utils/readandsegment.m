0839257d00b2346b9210b541ba52a1af08870fee
sahand92 <sahand92@gmail.com>
2019-10-06 01:31:41 +1000

    development
    add moisture predictions

--- +utils/readandsegment.m
+++ +utils/readandsegment.m
@@ -1,9 +1,11 @@
 
-function JJ = readandsegment(T,wavelengths)
+function JJ = readandsegment(T,wavelengths,folder)
 %segments multiple files and stacks the segments in a single array (JJ)
 % T: Table of sample names, where filenames are stored in column called 'Filename'
 % wavelengths: array index of wavelengths used, e.g., [72,72*2,72*3] for
-% RGB.
+% RGB. or [24:24:288-24] for class predictions
+%Folder in which subfolders contain the .raw measurement files:
+%e.g.: 'E:\Hyperspectral Data\Pulse Market Class\Group_1\'
 
 % Output JJ: 4-D image array. First 2 dims are the image pixels, dim 3 is 
 % the channel, and dim 4 corresponds to samples.
@@ -12,7 +14,7 @@ for ii = 1:length(T.Filename)
     strcat('file:',{' '},string(ii),{' '},'of',{' '},...
         string(length(T.Filename))) %progress
    
-    filename = strcat('E:\Hyperspectral Data\Pulse Market Class\Group_1\',...
+    filename = strcat(folder,...
     T.Filename(ii),'\measurement.raw');  
 
     D=utils.readHSIraw(filename);
